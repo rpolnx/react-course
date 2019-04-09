ffcf1cb199dffc62dcee2ee4fdb30a284833ad8c
Rodrigo Pereira <rodrigorpogo@gmail.com>
2019-04-09 00:19:17 -0300

    Router project to test and keeping the udemy classes

--- react-basics-stack/src/App.js
+++ react-basics-stack/src/App.js
@@ -1,39 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  // Special element on react -- if it's changes, react will re-render
-  state = {
+const App = props => {
+  // Special element on react -- if it's changes, react will re-render => state old way
+  // With functions, pure js can be more used on React
+  // Destructing: first element is the data, second is the function to set state, now can be customized
+  const [personsState, setPersonsState] = useState({
     persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 26}
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value'
+  });
+
+  //An elegant way is to define a variable const if you don't want to change it and don't want to be mutated
+
+  const switchNameHandler = (newName) => {
+    console.log('Was clicked!');
+    // This do not merge params with the passed one!!!
+    setPersonsState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manuela', age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ],
+      otherState: personsState.otherState
+    });
   }
 
-  switchNameHandler = () => {
-    console.log('Was clicked!');
-    this.setState({persons: [
-        {name: 'Maximilian', age: 28},
-        {name: 'Manuela', age: 29},
-        {name: 'Stephanie', age: 26}
+  const nameChangeHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 }, /* The input of got name */
+        { name: 'Stephanie', age: 26 }
       ]
     });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name}  age={this.state.persons[2].age}/>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi!</h1>
+      <button onClick={switchNameHandler.bind(this, "Maxs")}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} click={switchNameHandler.bind(this, "Julia")}
+        changed={nameChangeHandler}>
+        My hobbies: Racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
 }
 
 export default App;
---
+++ router/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <Navigation />
+        <Switch> {/* Without Switch we can't use error */}
+          <Route path="/" component={Home} exact /> {/* This tells to find the exact */}
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Error}></Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
