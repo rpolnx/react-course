e7a67a6867b6bef1c285462b4717f3cc5a767641
Rodrigo Pereira <rodrigorpogo@gmail.com>
2021-07-24 12:37:33 -0300

    React callback and React memo optimization

--- optimization-techniques/01-starting-setup/src/App.js
+++ optimization-techniques/01-starting-setup/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
@@ -6,19 +6,32 @@ import Button from "./components/UI/Button/Button";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("render parent");
 
-  const toggle = () => {
-    setShowParagraph(!showParagraph);
-  };
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+      // setShowParagraph(!showParagraph); -- needs to use previously state
+    }
+  }, [allowToggle]); // needs to pass value evaluation as dependency or it will never re-execute
 
+  const allowToggleHandler = useCallback(() => {
+    setAllowToggle((allowToggle) => !allowToggle);
+  }, []);
+
+  // useCallback(toggle, [showParagraph]);
+
+  // React.memo compare previously values, but functions always change references when created for components
+  // to solve this, useCallback can memorize previously function
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {showParagraph && <p>Show paragraph on parent!</p>}
       <DemoOutput show={showParagraph} />
-      <Button onClick={toggle}>Toggle Paragraph!</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+      <Button onClick={allowToggleHandler}>Allow toggle!</Button>
     </div>
   );
 }
